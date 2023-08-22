9537a5136a56da677b2919742108ed01d4d43222
ALLUK <lam83@bk.ru>
2023-08-22 21:22:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, type a number task: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number==1)
+            {
+                OneTaskApp();
+            }
+            else
+            {
+                Console.WriteLine("Число не введено!");
+            }
+            Console.ReadLine();
+        }
+        ///<summary>
+        ///Выведите обратную последовательность нечетных чисел на отрезке от a до b, где неизвестно какая переменная больше.
+        ///</summary>
+        
+        static void OneTaskApp()
+        {
+            Console.Write("Please, enter any number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, enter another number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int maxNumber=Math.Max(firstNumber, secondNumber);
+            int minNumber=Math.Min(firstNumber, secondNumber);
+
+            for (int i = maxNumber; i >= minNumber; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
